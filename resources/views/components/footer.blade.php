9e316b648a961eed2811b8f6f724ed063f3d7362
Juwel-771 <juwelah99@gmail.com>
2022-01-18 15:28:35 +0600

    Footer Section Created

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,56 @@
+<div class="container">
+    <div class="pt-20">
+        <h2 class="section-heading">Article & Advice</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div class="bg-gray-500" style="min-width: 130px"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div class="bg-gray-500" style="min-width: 130px"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div class="bg-gray-500" style="min-width: 130px"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pt-20 text-lg border-b-2 border-blue-400 pb-16 mb-20">
+        <h2 class="section-heading">Aditional Services</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Lorem, ipsum dolor.</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Lorem, ipsum dolor.</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Lorem, ipsum dolor.</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Lorem, ipsum dolor.</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Lorem, ipsum dolor.</a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-sans text-gray-900 antialiased bg-gray-100 min-h-screen">
             @include('components.header')
             {{ $slot }}
             @include('components.footer')
