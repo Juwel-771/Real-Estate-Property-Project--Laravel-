6aa363264826ec42f3dac3328e6ec6b2c07ae8af
Juwel-771 <juwelah99@gmail.com>
2022-01-18 14:03:38 +0600

    Header Section

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<div class="fixed w-full py-4 px-12 flex flex-wrap justify-between items-center">
+    <div class="min-w-max">
+        <img src="img/logo.png" width="100" alt="">
+        <div class="min-w-max">
+            <a href="">en</a>
+            <a href="">tr</a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-guest-layout>
+    <h1>test</h1>
+</x-guest-layout>
