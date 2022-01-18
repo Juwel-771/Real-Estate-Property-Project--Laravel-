d242aaa18c8d9c31f45a97667c0d3af035220d95
Juwel-771 <juwelah99@gmail.com>
2022-01-18 14:45:18 +0600

    Header Section Completed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,20 @@
-<div class="fixed w-full py-4 px-12 flex flex-wrap justify-between items-center">
+<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30">
     <div class="min-w-max">
-        <img src="img/logo.png" width="100" alt="">
-        <div class="min-w-max">
-            <a href="">en</a>
-            <a href="">tr</a>
-        </div>
+        <img src="img/logo.png" width="50" alt="">
+    </div>
+
+    <div class="w-full">
+        <ul class="flex justify-center">
+            <li><a class="inline-block p-4 text-white" href="">Land</a></li>
+            <li><a class="inline-block p-4 text-white" href="">Villa</a></li>
+            <li><a class="inline-block p-4 text-white" href="">Apartment</a></li>
+            <li><a class="inline-block p-4 text-white" href="">About Us</a></li>
+            <li><a class="inline-block p-4 text-white" href="">Contact Us</a></li>
+            </ul> 
+    </div>
+    
+    <div class="min-w-max">
+        <a href="">en</a>
+        <a href="">tr</a>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 <x-guest-layout>
-    <h1>test</h1>
+    <div class="relative z-10 pt-52 pb-64 bg-cover bg-center" style="background-image: url(img/hero.jpg)">
+        <div class="absolute h-full w-full bg-black opacity-60 top-0 left-0 z-10"></div>
+        <div class="container relative z-20 text-white text-center text-2xl">
+            <h2 class="font-bold text-5xl mb-8">Lorem ipsum, dolor sit amet <br> consectetur adipisicing elit. Autem, culpa?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
 </x-guest-layout>
+ 
